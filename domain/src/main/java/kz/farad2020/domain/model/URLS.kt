40037248c45b03e9d479a7606a9d641f0bcf8d9e
Sillicium <spotttiework@gmail.com>
2024-10-09 15:58:38 +0500

    rm TODO

--- domain/src/main/java/kz/farad2020/domain/model/URLS.kt
+++ domain/src/main/java/kz/farad2020/domain/model/URLS.kt
@@ -1,7 +1,6 @@
 package kz.farad2020.domain.model
 
 object AppBaseUrls{
-//    TODO move links and etc to buildConfigs
     const val GITHUB_API_URL = "https://api.github.com"
 
     const val GITHUB_URL = "https://github.com"
